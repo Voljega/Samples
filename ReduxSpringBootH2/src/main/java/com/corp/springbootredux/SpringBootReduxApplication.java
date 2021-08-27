4d9d7e1c4316ad5de851d53b81a61d0eda01cddc
Voljega <volega@gmail.com>
2021-08-27 18:41:16 +0200

    Second sample project

---
+++ ReduxSpringBootH2/src/main/java/com/corp/springbootredux/SpringBootReduxApplication.java
@@ -0,0 +1,11 @@
+package com.corp.springbootredux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReduxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReduxApplication.class, args);
+    }
+}
